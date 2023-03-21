bd13b2ebee4c8d75500adcb6bf98cb2eb59861c4
樺 <109811435+Jie-hua-Bao@users.noreply.github.com>
2023-03-21 23:43:06 +0800

    feat 新增FollowBtn UserPopuartBar，db假資料 (#5)
    * feat Add UserLoginPage 登入頁面
    
    * feat 新增FollowBtn UserPopuartBar，db假資料

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLoginPage from "./Pages/UserLoginPage";
 import RegisterPage from "./Pages/RegisterPage";
 import AdminLoginPage from "./Pages/AdminLoginPage";
+
+import UserPopularBar from "./Components/UserPopularBar";
+
 const basename = process.env.PUBLIC_URL;
 function App() {
   return (
@@ -12,9 +15,12 @@ function App() {
       <BrowserRouter basename={basename}>
         <Routes>
           <Route path="/">
+            {/* 暫時先設定為login，還沒使用useNavigate跳轉 */}
+            <Route index element={<UserLoginPage />}></Route>
             <Route path="login" element={<UserLoginPage />}></Route>
             <Route path="admin" element={<AdminLoginPage />}></Route>
             <Route path="register" element={<RegisterPage />}></Route>
+            <Route path="test" element={<UserPopularBar />}></Route>
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/Components/UserPopularBar/UserPopularList/UserPopularCard/index.jsx
@@ -0,0 +1,35 @@
+import styles from "./UserPopularCard.module.scss";
+import DefaultAvatar from "../../../../assets/icons/AcLogo.svg";
+import { Link } from "react-router-dom";
+import UserFollowBtn from "../../../UserFollowBtn";
+function PopularUserCard({
+  userName,
+  accountName,
+  avatar,
+  userID,
+  isFollowed,
+  setTopFollower,
+}) {
+  return (
+    <div className={styles["container"]}>
+      <Link to={`/user/${userID}`}>
+        <img
+          src={avatar ? avatar : DefaultAvatar}
+          alt="user-avatar"
+          className={styles["user-avatar"]}
+        />
+      </Link>
+      <div className={styles["user-info"]}>
+        <p className={styles["user-name"]}>
+          {userName ? userName : "Pizza Hut"}
+        </p>
+        <p className={styles["user-account"]}>
+          {accountName ? `@${accountName}` : "@pizzahut"}
+        </p>
+      </div>
+
+      <UserFollowBtn />
+    </div>
+  );
+}
+export default PopularUserCard;
