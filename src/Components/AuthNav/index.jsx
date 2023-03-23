72945aa610146e1bc85258101987ef7da9ec94d3
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-03-24 05:42:27 +0800

    feat: using useContext, useEffect, useState setup login state

--- src/Components/AuthNav/index.jsx
+++ src/Components/AuthNav/index.jsx
@@ -1,11 +1,28 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 
-function AuthNav({ Login }) {
+import { Myaxios } from '../../constants'
+
+
+function AuthNav({ token, setToken }) {
+	const localToken = localStorage.getItem('token') //測試過可以取出
+	
 	const navigate = useNavigate()
 	useEffect(() => {
-		if (!Login) navigate('/login')
-	}, [Login])
+		if (!localToken) return navigate('/login')
+		Myaxios(localToken)
+			.post('/users/test-token')
+			.then(e => {
+				if (e.data.status === 'success') {
+					setToken(localToken)
+					navigate('/home')
+				}
+			})
+			.catch(e => {
+				setToken('')
+				navigate('/login')
+			})
+	}, [])
 
 	return (
 		<div>
