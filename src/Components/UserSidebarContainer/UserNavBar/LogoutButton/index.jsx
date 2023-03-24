e9625cf95bda83b92795f9b7c53b8c139d3dcd1d
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-03-24 11:49:23 +0800

    feat: logout funciton

--- src/Components/UserSidebarContainer/UserNavBar/LogoutButton/index.jsx
+++ src/Components/UserSidebarContainer/UserNavBar/LogoutButton/index.jsx
@@ -1,16 +1,24 @@
+
 import styles from "./LogoutButton.module.scss";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { ReactComponent as Logout } from "../../../../assets/icons/logout.svg";
 
 function LogoutButton() {
+  const navigate = useNavigate()
+  const logout = (e) => {
+    console.log(e.target)
+    navigate('/login')
+    localStorage.removeItem('token')
+    
+  }
   return (
-    <div className={styles["container"]}>
-      <NavLink className={styles["navlink-logo"]}>
-        <Logout />
-      </NavLink>
-      <NavLink className={styles["navlink-title"]}>登出</NavLink>
-    </div>
-  );
+		<div className={styles['container']} onClick={logout}>
+			<NavLink className={styles['navlink-logo']}>
+				<Logout />
+			</NavLink>
+			<NavLink className={styles['navlink-title']}>登出</NavLink>
+		</div>
+	)
 }
 
 export default LogoutButton;
