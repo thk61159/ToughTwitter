d3f3a77551b90b935722233d3ed11d91f988978a
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-03-24 23:31:34 +0800

    Squashed commit of the following:
    commit 2593934c8bf431ec4abddc89f7f13aedc6f64706
    Author: 樺 <xd_301@yahoo.com.tw>
    Date:   Thu Mar 23 23:23:31 2023 +0800
    
        Modify 修改 home 路由 ，個人頁面

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,8 @@ import RegisterPage from './Pages/RegisterPage'
 import AdminLoginPage from './Pages/AdminLoginPage'
 import SettingPage from './Pages/SettingPage'
 import LayoutUser from './Components/LayoutUser'
-import TweetInput from './Components/TweetInput'
 import HomePage from './Pages/HomePage'
-
+import UserProfilePage from'./Pages/UserProfilePage'
 //components
 import AuthNav from './Components/AuthNav'
 
@@ -19,28 +18,32 @@ const basename = process.env.PUBLIC_URL
 function App() {
 	let [Login, setLogin] = useState(false)
 	return (
-		<div className={styles.App}>
-			<BrowserRouter basename={basename}>
-				<Routes>
-					{/* 用一個nav去處理確認登入狀態的事件 ， 非登入者就重新導向到 /login*/}
-					<Route path="/" element={<AuthNav Login={ Login } />}>
-						{/* 暫時先設定為login，還沒使用useNavigate跳轉 */}
-						{/* <Route index element={<UserLoginPage />}></Route> */}
-						<Route path="login" element={<UserLoginPage setLogin={ setLogin } />}></Route>
-						<Route path="admin" element={<AdminLoginPage />}></Route>
-						<Route path="register" element={<RegisterPage />}></Route>
-						<Route path="setting" element={<SettingPage />}></Route>
-						{/* 後端還沒用top followers所以先不管LayoutUser */}
-						<Route path="tweet/:id" element={<LayoutUser />}>
-							<Route index element={<HomePage />}></Route>
-						</Route>
-
-						<Route path="test" element={<HomePage />}></Route>
-					</Route>
-				</Routes>
-			</BrowserRouter>
-		</div>
-	)
+    <div className={styles.App}>
+      <BrowserRouter basename={basename}>
+        <Routes>
+          {/* 用一個nav去處理確認登入狀態的事件 ， 非登入者就重新導向到 /login*/}
+          <Route path="/" element={<AuthNav Login={Login} />}>
+            {/* 暫時先設定為login，還沒使用useNavigate跳轉 */}
+            {/* <Route index element={<UserLoginPage />}></Route> */}
+            <Route
+              path="login"
+              element={<UserLoginPage setLogin={setLogin} />}
+            ></Route>
+            <Route path="admin" element={<AdminLoginPage />}></Route>
+            <Route path="register" element={<RegisterPage />}></Route>
+            <Route path="setting" element={<SettingPage />}></Route>
+            {/* 後端還沒用top followers所以先不管LayoutUser */}
+            <Route path="home" element={<LayoutUser />}>
+              <Route index element={<HomePage />}></Route>
+            </Route>
+            <Route path="/user/:username" element={<LayoutUser />}>
+              <Route index element={<UserProfilePage />}></Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App
