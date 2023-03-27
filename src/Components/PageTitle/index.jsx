77c4e33059cff36760b3a788cc797bf2f9ff1dab
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-03-27 23:50:01 +0800

    feat: tweet modal and tweet post api

--- src/Components/PageTitle/index.jsx
+++ src/Components/PageTitle/index.jsx
@@ -1,17 +1,26 @@
-import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { useNavigate, useParams, useLocation } from 'react-router-dom'
 import styles from './PageTitle.module.scss'
+import MyContext from '../MyContext'
 import { ReactComponent as ArrowPre } from '../../assets/icons/arrowPre.svg'
 
 function PageTitle({ d }) {
 	const navigate = useNavigate()
+	const path = useLocation().pathname
+	const { account } = useParams()
+	const { user } = useContext(MyContext)
+	const previousPage = () => {
+		if (account == user.id && account == path.slice(1, path.length)) {
+			navigate('/home')
+		}
+		else {
+			navigate(`/${user.id}`)
+		}
+	}
 	return (
 		<div className={styles['container']}>
 			<div className={styles['arrow-img']}>
-				<ArrowPre
-					onClick={() => {
-						navigate(-1)
-					}}
-				/>
+				<ArrowPre onClick={previousPage} />
 			</div>
 			<div className={styles['profile-title']}>
 				<div className={styles['user-name']}>{d.name}</div>
--- src/Components/TweetInput/index.jsx
+++ src/Components/TweetInput/index.jsx
@@ -1,27 +1,55 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { Link } from 'react-router-dom'
 
 import styles from './TweetInput.module.scss'
 import MyContext from '../MyContext'
+import { Myaxios } from '../../constants'
 import TweetSubmitButton from './TweetSubmitButton'
 
-function TweetInput() {
-	const { user } = useContext(MyContext)
-	let [tweet,setTweet]=useState()
+function TweetInput({ setPost }) {
+	const { token, user } = useContext(MyContext)
+	let [tweet, setTweet] = useState(null)
+	let [error, setError] = useState(null)
+	const submitTweet = () => {
+		if (!tweet) return setError('不可空白')
+		if (tweet.length < 140 || !tweet.trim()) {
+			console.log('送出')
+			Myaxios(token)
+				.post(`/tweets`, { description: tweet })
+				.then(e => {
+					setPost(true)
+					setTweet('')
+					console.log('貼文送出', e.status)
+				})
+				.catch(err => console.log(err))
+		} else {
+			setError('字數不可超過140字或是空白')
+		}
+	}
+	useEffect(() => {
+		if (tweet) {
+			if (tweet.length > 140) {
+				return setError('字數不可超過140字')
+			} else if (!tweet.trim()) {
+				return setTweet('')
+			} else {
+				setError(null)
+			}
+		}
+	}, [tweet])
 	return (
 		<div className={styles['container']}>
 			<div className={styles['input-body']}>
 				<div className={styles['user-avatar']}>
-			
-						<img src={user.avatar} alt='avatar-img' className={styles['avatar-img']} />
-					
+					<img src={user.avatar} alt='avatar-img' className={styles['avatar-img']} />
 				</div>
 				<textarea className={styles['input-textarea']} placeholder='有什麼新鮮事?' onChange={e => setTweet(e.target.value)} value={tweet}></textarea>
 			</div>
 			<div className={styles['footer']}>
-				<div className={styles['error-message']}></div>
+				<div className={styles['error-message']}>{error}</div>
 				<div className={styles['tweet-btn']}>
-					<TweetSubmitButton />
+					<button className={styles['btn-submit']} onClick={submitTweet}>
+						<TweetSubmitButton />
+					</button>
 				</div>
 			</div>
 		</div>
