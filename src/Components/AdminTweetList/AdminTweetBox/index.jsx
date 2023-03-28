2c2c837b69ac8229f9eab052ca0eee473b20deb3
Noelle-KH <noelletang86@gmail.com>
2023-03-28 14:25:04 +0800

    feat: add admin tweets page and components

---
+++ src/Components/AdminTweetList/AdminTweetBox/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './AdminTweetBox.module.scss';
+import { ReactComponent as DeleteIcon } from '../../../assets/icons/admin_delete_icon.svg';
+
+const AdminTweetBox = ({ id, avatar, name, account, time, description, deleteTweet }) => {
+
+  return (
+    <div className={styles['tweet-container']}>
+      <div className={styles['tweet-image-container']}>
+        <img src={avatar} alt='avatar' />
+      </div>
+      <div className={styles['tweet-content']}>
+        <p className={styles['tweet-user-info']}>
+          <span>{name}</span>
+          <span>@{account}・{time}</span>
+        </p>
+        <p className={styles['tweet-description']}>{description}</p>
+      </div>
+      <DeleteIcon onClick={() => deleteTweet(id)} />
+    </div>
+  );
+};
+
+export default AdminTweetBox;
