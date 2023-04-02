d07e56b577c0ccdfcd2041df34eac64225581707
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-04-03 01:47:09 +0800

    fix: setting svg active

--- src/Components/UserSidebarContainer/UserNavBar/index.jsx
+++ src/Components/UserSidebarContainer/UserNavBar/index.jsx
@@ -28,45 +28,47 @@ function UserNavBar() {
 	// }, [userData])
 	return (
 		<>
-			{user&&<div className={styles['container']}>
-				<AcLogo className={styles['navbar-logo']} />
-				<NavBarItem>
-					<NavLink to={'/home'} className={({ isActive }) => [`${styles['navbar-link']}`, isActive ? `${styles['router-link-active']}` : ``].join(' ')} end>
-						<House className={styles['navbar-link__logo']} />
-						<HouseActive className={styles['navbar-link__logo-active']} />
-					</NavLink>
-					<NavLink to={'/home'} className={({ isActive }) => [`${styles['navbar-link']}`, isActive ? `${styles['router-link-active']}` : ``].join(' ')} end>
-						{' '}
-						<p className={styles['navbar-link__title']}>首頁</p>
-					</NavLink>
-				</NavBarItem>
-				<NavBarItem>
-					<NavLink to={`/${user.id}`} className={({ isActive }) => [`${styles['navbar-link']}`, isActive ? `${styles['router-link-active']}` : ``].join(' ')}>
-						<Head className={styles['navbar-link__logo']} />
-						<HeadActive className={styles['navbar-link__logo-active']} />
-						<p className={styles['navbar-link__title']}>個人資料</p>
-					</NavLink>
-				</NavBarItem>
-				<NavBarItem>
-					<NavLink to={'/setting'} className={styles['navbar-link']} end>
-						<Gear className={styles['navbar-link__logo']} />
-						<GearActive className={styles['navbar-link__logo-active']} />
-					</NavLink>
-					<NavLink to={'/setting'} className={({ isActive }) => [`${styles['navbar-link']}`, isActive ? `${styles['router-link-active']}` : ``].join(' ')} end>
-						<p className={styles['navbar-link__title']}>設定</p>
-					</NavLink>
-				</NavBarItem>
-				<div
-					className={styles['remove-btn']}
-					onClick={() => {
-						setModal(!Modal)
-					}}>
-					<TweetButtonSideBar />
-				</div>
+			{user && (
+				<div className={styles['container']}>
+					<AcLogo className={styles['navbar-logo']} />
+					<NavBarItem>
+						<NavLink to={'/home'} className={({ isActive }) => [`${styles['navbar-link']}`, isActive ? `${styles['router-link-active']}` : ``].join(' ')} end>
+							<House className={styles['navbar-link__logo']} />
+							<HouseActive className={styles['navbar-link__logo-active']} />
+						</NavLink>
+						<NavLink to={'/home'} className={({ isActive }) => [`${styles['navbar-link']}`, isActive ? `${styles['router-link-active']}` : ``].join(' ')} end>
+							{' '}
+							<p className={styles['navbar-link__title']}>首頁</p>
+						</NavLink>
+					</NavBarItem>
+					<NavBarItem>
+						<NavLink to={`/${user.id}`} className={({ isActive }) => [`${styles['navbar-link']}`, isActive ? `${styles['router-link-active']}` : ``].join(' ')}>
+							<Head className={styles['navbar-link__logo']} />
+							<HeadActive className={styles['navbar-link__logo-active']} />
+							<p className={styles['navbar-link__title']}>個人資料</p>
+						</NavLink>
+					</NavBarItem>
+					<NavBarItem>
+						<NavLink to={'/setting'} className={({ isActive }) => [`${styles['navbar-link']}`, isActive ? `${styles['router-link-active']}` : ``].join(' ')} end>
+							<Gear className={styles['navbar-link__logo']} />
+							<GearActive className={styles['navbar-link__logo-active']} />
+						</NavLink>
+						<NavLink to={'/setting'} className={({ isActive }) => [`${styles['navbar-link']}`, isActive ? `${styles['router-link-active']}` : ``].join(' ')} end>
+							<p className={styles['navbar-link__title']}>設定</p>
+						</NavLink>
+					</NavBarItem>
+					<div
+						className={styles['remove-btn']}
+						onClick={() => {
+							setModal(!Modal)
+						}}>
+						<TweetButtonSideBar />
+					</div>
 
-				<TweetInputModal Modal={Modal} setModal={setModal} />
-				<div className={styles['logout-btn']}></div>
-			</div>}
+					<TweetInputModal Modal={Modal} setModal={setModal} />
+					<div className={styles['logout-btn']}></div>
+				</div>
+			)}
 		</>
 	)
 }
