154cc4c1aac24e2751d7ce9e2c17bf6f05458d29
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-04-03 01:55:44 +0800

    fix: follower page following function

--- src/Components/UserFollowshipBox/index.jsx
+++ src/Components/UserFollowshipBox/index.jsx
@@ -10,23 +10,24 @@ import DefaultAvatar from '../../assets/icons/AcLogo.svg'
 
 
 function UserFollowshipBox({ d }) {
+	console.log(d)
 	
 	
 	return (
 		<div className={styles['container']}>
 			<div className={styles['user-avatar']}>
-				<Link to={`/${d.followingId}`}>
+				<Link to={`/${d?.followingId || d?.followerId}`}>
 					<img src={d.avatar || DefaultAvatar} className={styles['avatar-img']} alt='avatar-img' />
 				</Link>
 			</div>
 			<div className={styles['tweet-user-info']}>
 				<div className={styles['tweet-user-head']}>
 					<p className={styles['user-info-name']}>{d.name}</p>
-					<UserFollowBtn currentfollowed={d.currentfollowed} userId={d.followingId} />
+					<UserFollowBtn currentfollowed={d.currentfollowed} userId={d?.followingId || d?.followerId} />
 				</div>
 
 				<div className={styles['tweet-content']}>
-					<Link to={`/tweet/${d.followingId}`} className={styles['tweet-content-link']}>
+					<Link to={`/tweet/${d?.followingId || d?.followerId}`} className={styles['tweet-content-link']}>
 						<div>{d.introduction}</div>
 					</Link>
 				</div>
